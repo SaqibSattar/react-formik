050f7d06c7852bc191e99f302c24a65e116cbfdd
SaqibSattar <saqibkhanz1234@gmail.com>
2024-01-26 10:43:21 +0400

    handling form submission

--- src/components/YoutubeForm.jsx
+++ src/components/YoutubeForm.jsx
@@ -8,13 +8,14 @@ const YoutubeForm = () => {
       email: "",
       channel: "",
     },
+    onSubmit: (values) => {
+      console.log('Form values: ', values)
+    }
   });
-
-  console.log("Formik values: ", formik, formik.values);
 
   return (
     <div>
-      <form>
+      <form onSubmit={formik.handleSubmit}>
         <label htmlFor="name">Name</label>
         <input
           type="text"
@@ -42,7 +43,7 @@ const YoutubeForm = () => {
           value={formik.values.channel}
         />
         <br />
-        <button>Submit</button>
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
