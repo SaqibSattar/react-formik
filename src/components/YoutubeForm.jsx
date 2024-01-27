ecc29a1d76335efd9f9c1393eb1c00cd0a6fb3b4
SaqibSattar <saqibkhanz1234@gmail.com>
2024-01-27 11:05:13 +0400

    ErrorMessage Revisited

--- src/components/YoutubeForm.jsx
+++ src/components/YoutubeForm.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
+import TextError from "./TextError";
 
 const initialValues = {
   name: "",
@@ -19,7 +20,7 @@ const validationSchema = Yup.object({
   email: Yup.string().email("Invalid email format").required("Required!"),
   channel: Yup.string().required("Required!"),
   comments: Yup.string().required("Required!"),
-  address: Yup.string().required("Required!")
+  address: Yup.string().required("Required!"),
 });
 
 const YoutubeForm = () => {
@@ -33,12 +34,16 @@ const YoutubeForm = () => {
         <div className="form-control">
           <label htmlFor="name">Name</label>
           <Field type="text" id="name" name="name" />
-          <ErrorMessage name="name" />
+          <ErrorMessage name="name" component={TextError} />
         </div>
         <div className="form-control">
           <label htmlFor="email">Email</label>
           <Field type="email" id="email" name="email" />
-          <ErrorMessage name="email" />
+          <ErrorMessage name="email">
+            {
+              (errorMsg) => <div className="error">{errorMsg}</div>
+            }
+          </ErrorMessage>
         </div>
         <div className="form-control">
           <label htmlFor="channel">Channel</label>
