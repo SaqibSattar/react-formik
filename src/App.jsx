2ede17683cbc16e9b5a73432edf4702640e2fb8c
SaqibSattar <saqibkhanz1234@gmail.com>
2024-01-31 11:35:46 +0400

    Registration Form

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import "./App.css";
 import YoutubeForm from "./components/YoutubeForm";
 import FormikContainer from "./components/FormikContainer";
 import LoginForm from "./components/LoginForm";
+import RegistrationForm from "./components/RegistrationForm";
 
 function App() {
   return (
     <div className="App">
     {/* <YoutubeForm /> 
-    <FormikContainer /> */}
-    <LoginForm />
+    <FormikContainer />
+    <LoginForm /> */}
+    <RegistrationForm />
     </div>
   );
 }
