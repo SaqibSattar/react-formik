ce3d96e8eb7de685bf6717b8fa15bf25f90e2dd1
SaqibSattar <saqibkhanz1234@gmail.com>
2024-01-31 11:47:55 +0400

    Course Enrollment Form

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import YoutubeForm from "./components/YoutubeForm";
 import FormikContainer from "./components/FormikContainer";
 import LoginForm from "./components/LoginForm";
 import RegistrationForm from "./components/RegistrationForm";
+import EnrollmentForm from "./components/EnrollmentForm";
 
 function App() {
   return (
     <div className="App">
     {/* <YoutubeForm /> 
     <FormikContainer />
-    <LoginForm /> */}
-    <RegistrationForm />
+    <LoginForm />
+    <RegistrationForm /> */}
+    <EnrollmentForm />
     </div>
   );
 }
